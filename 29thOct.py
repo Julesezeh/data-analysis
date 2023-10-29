193117b0fb30a36479e17af2157c009080d87bc4
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-29 21:26:17 +0100

    increment operator added

--- 29thOct.py
+++ 29thOct.py
@@ -6,6 +6,7 @@ boston = pd.read_html("https://www.basketball-reference.com/teams/BOS/2024.html"
 
 # for tables in boston:
 #     print(tables)
-file_number = 0
+file_number = 1
 for tables in boston:
     tables.to_csv(f"boston{file_number}.csv")
+    file_number+=1
