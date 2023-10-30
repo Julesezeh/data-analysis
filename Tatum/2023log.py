37433bac2de2d5fc5d95ff15ffbb9e0100fe803f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-30 10:05:26 +0100

    3p stats against wizards

--- Tatum/2023log.py
+++ Tatum/2023log.py
@@ -5,5 +5,5 @@ data = pd.read_csv("tatum7.csv")
 # shooting_data = data[["3P","3PA","Date"]]
 shooting_data = data.loc[:,["Opp","3P","3PA"]]
 
-print(shooting_data.loc[shooting_data["Opp"] =="PHI"])
+print(shooting_data.loc[shooting_data["Opp"] =="WAS"])
 # print(data.columns.to_list())
