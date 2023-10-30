339c2aa078a6de46d50f7cb24cbc602b5ddee9ee
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-31 00:14:06 +0100

    fetching tatum stats

--- App/Tatum/fetchTatumStats.py
+++ App/Tatum/fetchTatumStats.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-logs = {"2023":"https://www.basketball-reference.com/players/s/stewais01/gamelog/2023","recent":"https://www.basketball-reference.com/players/s/stewais01/gamelog/2024"}
+logs = {"2023":"https://www.basketball-reference.com/players/t/tatumja01/gamelog/2023","recent":"https://www.basketball-reference.com/players/t/tatumja01/gamelog/2024"}
 
 
 for urls in logs.items():
     data = pd.read_html(urls[1])
     if urls[0]=='2023':
-        data[7].to_csv("Stewart2023.csv")
+        data[7].to_csv("Tatum2023.csv")
     elif urls[0]=='recent':
-        data[7].to_csv("StewartRecent.csv")
+        data[7].to_csv("TatumRecent.csv")
 
