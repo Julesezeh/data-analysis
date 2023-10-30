1e6ed7c3d257e5f007cbd129ff0539660cb6edfe
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-30 16:32:44 +0100

    Saving recent and 2023 logs. comment out 2023.

---
+++ App/Doncic/fetch_2023_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+logs = {"2023":"https://www.basketball-reference.com/players/d/doncilu01/gamelog/2023","recent":"https://www.basketball-reference.com/players/d/doncilu01/splits/2024"}
+
+
+for urls in logs.items():
+    data = pd.read_html(urls[1])
+    if urls[0]=='2023':
+        data[7].to_csv("Luka2023.csv")
+    elif urls[0]=='recent':
+        data[0].to_csv("LukaRecent.csv")
+
