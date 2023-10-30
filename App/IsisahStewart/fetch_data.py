0c05f521b6290025b220d956684f0be4938ae4f4
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-30 16:53:35 +0100

    Fetching 2023 and recent Isiah Stewart

---
+++ App/IsisahStewart/fetch_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+logs = {"2023":"https://www.basketball-reference.com/players/s/stewais01/gamelog/2023","recent":"https://www.basketball-reference.com/players/s/stewais01/gamelog/2024"}
+
+
+for urls in logs.items():
+    data = pd.read_html(urls[1])
+    if urls[0]=='2023':
+        data[7].to_csv("Stewart2023.csv")
+    elif urls[0]=='recent':
+        data[0].to_csv("StewartRecent.csv")
+
