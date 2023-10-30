a70ca6a3b8709f003df7460d13357913585dac53
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-30 12:23:29 +0100

    Moved the files

--- App/SGA/fetch_2023_stats.py
+++ App/SGA/fetch_2023_stats.py
@@ -1,5 +1,9 @@
-from Tools.fetch_stats import fetch_stats
+import pandas as pd
 
-data = fetch_stats("https://www.basketball-reference.com/players/g/gilgesh01/gamelog/2023")
 
-print(data)
+data = pd.read_html("https://www.basketball-reference.com/players/g/gilgesh01/gamelog/2023")
+
+number =  0
+for datum in data:
+    datum.to_csv(f"shai{number}.csv")
+    number +=1
