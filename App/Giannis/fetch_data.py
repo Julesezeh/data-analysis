1e0e4df3a8d843bd67e709301406a9f11fcac274
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-31 00:11:26 +0100

    fetch giannis stats

---
+++ App/Giannis/fetch_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+logs = {"2023":"https://www.basketball-reference.com/players/a/antetgi01/gamelog/2023","recent":"https://www.basketball-reference.com/players/a/antetgi01/gamelog/2024"}
+
+
+for urls in logs.items():
+    data = pd.read_html(urls[1])
+    if urls[0]=='2023':
+        data[7].to_csv("Giannis2023.csv")
+    elif urls[0]=='recent':
+        data[7].to_csv("GiannisRecent.csv")
+
