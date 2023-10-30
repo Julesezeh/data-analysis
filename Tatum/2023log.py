44115cf81a72094e6070304f3e48a0b90eddd5f9
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-30 10:04:21 +0100

    Got philly data successfully.

--- Tatum/2023log.py
+++ Tatum/2023log.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-data = pd.read_csv("tatum7.csv", index_col="Opp")
+data = pd.read_csv("tatum7.csv")
 
-shooting_data = data[["3P","3PA","Date"]]
+# shooting_data = data[["3P","3PA","Date"]]
+shooting_data = data.loc[:,["Opp","3P","3PA"]]
 
-
-
-print(shooting_dat)
+print(shooting_data.loc[shooting_data["Opp"] =="PHI"])
+# print(data.columns.to_list())
