c8f731c63c77cfeaf79f143fd95423879828d09f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-31 13:52:59 +0100

    Fetch team stats for specified team in the gulag

---
+++ App/TheGulag/fetch_Team_Stats.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+teams={"ATL":"HAWKS","BOS":"CELTICS","BKN":"NETS","CHA":"HORNETS","CHI":"BULLS","CLE":"CAVS","DAL":"MAVS","DEN":"NUGGETS","DET":"PISTONS","GSW":"WARRIORS","HOU":"ROCKETS","IND":"PACERS","LAC":"CLIPPERS","LAL":"LAKERS","MEM":"GRIZZLIES","MIA":"HEAT","MIL":"BUCKS","MIN":"TIMBERWOLVES","NOL":"PELICANS","NYK":"KNICKS","OKC":"THUNDER","ORL":"MAGIC","PHI":"76ERS","PHX":"SUNS","POR":"TRAILBLAZERS","SAC":"KINGS","SAS":"SPURS","TOR":"RAPTORS","UTA":"JAZZ","WAS":"WIZARDS"}
+
+print(len(teams))
+
+team = input("What team's data do you need ? (write city code):> ").upper()
+
+
+if team in teams.keys():
+    team_url = input("URL:> ")
+    # year = input("year?:> ")
+    try:
+        data = pd.read_html(team_url)
+        year = re.search(r"\d{4}",team_url).group()
+        print("worked",year)
+
+        count =  0
+        for datum in data:
+            datum.to_csv(team+year+str(count)+"_stats.csv")
+            count+=1
+
+    except:
+         print("Error")
+
+
+# print()
