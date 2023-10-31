51258e008a1af71ec07957fb637227c2bb1f1a5d
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-31 12:52:37 +0100

    using 2023 data

--- App/Boston/use2023data.py
+++ App/Boston/use2023data.py
@@ -6,15 +6,18 @@ data =  pd.read_csv("Boston_regular_season_2023.csv")
 # print(data)
 
 
-scores = data.loc[:,["Opponent","Tm","Opp"]]
+scores = data.loc[:,["Opponent","Tm","Opp","Date"]]
 
 
 opponent = input("Opp?")
 
 opp_scores = scores.loc[scores['Opponent']==opponent]
 
-opp_scores["total"] = int(scores["Tm"]) + int(scores["Opp"])
+# scores["Tm"] = [int(x) for x in scores["Tm"]]
+
+#Summation works
+opp_scores["total"] = opp_scores[["Tm","Opp"]].astype(str).astype(int).sum(axis=1)
 
 print(opp_scores)
-# print(scores)
-# print(scores.loc[:,"Tm"])
+
+
