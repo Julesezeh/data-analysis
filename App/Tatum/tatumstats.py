58c4602935cb84d83e1aa67f9b00842f6a0f0c5b
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-01 12:23:54 +0100

    tweaking tatum stats

--- App/Tatum/tatumstats.py
+++ App/Tatum/tatumstats.py
@@ -4,17 +4,29 @@ data = pd.read_csv("Tatum2023.csv")
 
 # print(data)
 data.rename(
-    columns={"Unnamed: 5":"Home/Away" },
+    columns={"Unnamed: 5":"Home/Away","Unnamed: 7":"Win/Loss" },
     inplace=True
     )
 
-shooting_data = (data.loc[:,["Opp","3P","3PA","Home/Away"]])
+print(data.columns.to_list())
 
-opps = input("Opp? >")
+fg_shooting_data = (data.loc[:,["Date","PTS","FGA","FG","Opp"]])
 
-print(shooting_data.loc[shooting_data["Opp"] == opps])
+three_pts_shooting_data = (data.loc[:,["Date","PTS","3PA","3P","Opp"]])
+
+opps = input("Opp? >").upper()
+
+print(f"Three point shooting data against {opps}")
+print(three_pts_shooting_data.loc[three_pts_shooting_data["Opp"] == opps])
 
 
+print("\n\n\n\n\n")
 
+print(f"Field goals data against {opps}")
+opps_fg= fg_shooting_data.loc[fg_shooting_data["Opp"] == opps]
+print(opps_fg)
+
+
+print("\nAverage points scored",opps_fg["PTS"].astype(str).astype(int).mean())
 
 
