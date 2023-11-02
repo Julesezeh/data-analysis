87304984337995039d9ab2981030a0347ac4afe8
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-02 12:20:40 +0100

    correcting previous commit message ("Wizards")

--- App/Wizards/fetch_quarterly_splits.py
+++ App/Wizards/fetch_quarterly_splits.py
@@ -5,5 +5,5 @@ data = pd.read_html("https://www.basketball-reference.com/teams/WAS/2024/splits/
 
 count=0
 for datum in data:
-    datum.to_csv("Boston_splits_{count}.csv")
+    datum.to_csv("Wizards_splits_{count}.csv")
     count+=1
