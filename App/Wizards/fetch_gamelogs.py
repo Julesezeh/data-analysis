0a3d3239aa36904222cdd7c81008050522e29e58
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-02 13:41:45 +0100

    tweaked for 2024 game logs

--- App/Wizards/fetch_gamelogs.py
+++ App/Wizards/fetch_gamelogs.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-data = pd.read_html("https://www.basketball-reference.com/teams/WAS/2023_games.html")
+data = pd.read_html("https://www.basketball-reference.com/teams/WAS/2024_games.html")
 
-count = 0
 for datum in data:
-    datum.to_csv(f"Wizards{count}.csv")
+    datum.to_csv(f"Wizards2024.csv")
