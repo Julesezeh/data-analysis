4857672ecb9412245f39e395eb7755ef840bad80
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-11-11 21:23:30 +0100

    Update fetch_gamelogs.py

--- App/Wizards/fetch_gamelogs.py
+++ App/Wizards/fetch_gamelogs.py
@@ -3,4 +3,5 @@ import pandas as pd
 data = pd.read_html("https://www.basketball-reference.com/teams/WAS/2024_games.html")
 
 for datum in data:
+    print("file saved"
     datum.to_csv(f"Wizards2024.csv")
